7698cbcae338c4053d7acd8910222af12f4035cf
Kylin <kylinsoong.1214@gmail.com>
2014-04-27 16:51:27 +0800

    remove redundant code

--- msc/singleton/service/src/main/java/org/jboss/as/quickstarts/cluster/hasingleton/service/ejb/Scheduler.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.as.quickstarts.cluster.hasingleton.service.ejb;
-
-public interface Scheduler {
-
-    void initialize(String info);
-
-    void stop();
-
-}
--- perf/slsb/src/main/java/org/jboss/demo/cluster/slsb/StatelessSession.java
+++
@@ -1,5 +0,0 @@
-package org.jboss.demo.cluster.slsb;
-
-public interface StatelessSession {
-	public void invoke(int time);
-}
